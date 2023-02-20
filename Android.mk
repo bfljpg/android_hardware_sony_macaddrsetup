ed34e2ed528ca568a490ec448349ce552aaf0bcd
Emir Cankur <emircnkr66@gmail.com>
2023-02-20 18:29:58 +0300

    initial commit

---
+++ Android.mk
@@ -0,0 +1,37 @@
+ifeq ($(PRODUCT_PLATFORM_SOD),true)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    macaddrsetup.c
+
+ifeq ($(WIFI_DRIVER_BUILT),qca_cld3)
+LOCAL_CFLAGS += -DQCA_CLD3_WIFI
+endif
+
+ifeq ($(WIFI_DRIVER_BUILT),brcmfmac)
+LOCAL_CFLAGS += -DBRCMFMAC
+endif
+
+ifeq ($(BOARD_HAS_MIRROR_MACADDRESS),true)
+LOCAL_CFLAGS += -DMIRROR_MAC_ADDRESS
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils
+
+LOCAL_MODULE := macaddrsetup
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 25),)
+LOCAL_MODULE_OWNER := sony
+LOCAL_INIT_RC_64   := vendor/etc/init/macaddrsetup.rc
+LOCAL_PROPRIETARY_MODULE := true
+endif
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+endif
